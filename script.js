66cac6c551e2ff2790bac4f373db944e55a32b8d
mahir118 <mahiranand11@gmail.com>
2023-03-07 09:02:44 -0800

    Calculate and Update Display & Context Values in Real Time

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const displayValue = document.querySelector('#displayValue');
+const contextValue = document.querySelector('#contextValue');
 const nums = document.querySelectorAll('#num');
 const operators = document.querySelectorAll('#operation');
 const equal = document.querySelector('#equal');
@@ -6,6 +7,7 @@ const equal = document.querySelector('#equal');
 let curValue = 0;
 let prevValue = 0;
 let curOperator = "";
+let curOperatorText = "";
 
 for (let num of nums) {
     num.addEventListener('click', () => {
@@ -14,27 +16,28 @@ for (let num of nums) {
     })
 }
 
-
 for (let operator of operators) {
     operator.addEventListener('click', () => {
         if (prevValue == 0) {
             prevValue = Number(curValue);
             curValue = Number(0);
             curOperator = operator.value;
-            // displayValue.innerHTML = operator.innerHTML;
+            curOperatorText = operator.innerHTML;
+            contextValue.innerHTML = prevValue + operator.innerHTML;
         } else {
             prevValue = operate(prevValue, curValue, curOperator);
             curValue = Number(0);
             curOperator = operator.value;
-            // displayValue.innerHTML = operator.innerHTML;
+            curOperatorText = operator.innerHTML;
+            contextValue.innerHTML = prevValue + operator.innerHTML;
         }
         
     })
 }
 
 equal.addEventListener('click', () => {
-    console.log(prevValue, curValue, curOperator);
     operate(prevValue,curValue,curOperator);
+    contextValue.innerHTML = prevValue.toString() + curOperatorText + curValue.toString() + " = ";
 })
 
 
